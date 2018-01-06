466ea9a916801b8021b31fb437511b7182490e73
drunkenninja <drunken_ninja@hotmail.com>
2018-01-06 16:48:51 +0200

    Add Class

--- FF15 Auto Level.lua
+++ FF15 Auto Level.lua
@@ -1,4 +1,15 @@
-AutoLeveler = { }
+    local function class()
+           local cls = {}
+           cls.__index = cls
+           return setmetatable(cls, {__call = function (c, ...)
+               local instance = setmetatable({}, cls)
+               if cls.__init then
+                   cls.__init(instance, ...)
+               end
+               return instance
+           end})
+    end
+AutoLeveler = class()
 
 function AutoLeveler:Init()
 
